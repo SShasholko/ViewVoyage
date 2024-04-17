2d01e9c7daa6b2a8398c560a654d61c6bf34b84d
Svitlana Shasholko <shasholko.sveta@gmail.com>
2024-04-17 18:29:18 +0100

    Added fetching the images

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-// API from Pexels:    nWyUn0h0FmnDePqoeO86Iheim92k4G4nLW1zyGYuxA9KUHQKMiSlTX6y
-// Documentation Pexels API https://www.pexels.com/api/documentation/?utm_medium=email&utm_source=pexels&utm_campaign=API%20Onboarding&utm_content=welcome%20email
+const auth = 'nWyUn0h0FmnDePqoeO86Iheim92k4G4nLW1zyGYuxA9KUHQKMiSlTX6y'
+const gallery = document.querySelector('.gallery')
+const searchInput = document.querySelector('.search-input')
+const submitButton = document.querySelector('.submit-btn')
+let searchValue;
 
+async function curatedPhotos(){
+    const dataFetch = await fetch('https://api.pexels.com/v1/curated?per_page=15&page=1',{
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: auth
+        }
+    })
+    const data = await dataFetch.json()
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement('div')
+        galleryImg.classList.add('gallery-img')
+        galleryImg.innerHTML = `<img src=${photo.src.large}/>
+        <p>${photo.photographer}</p>`;
+        gallery.appendChild(galleryImg)
+    });
+}
+
+curatedPhotos()
